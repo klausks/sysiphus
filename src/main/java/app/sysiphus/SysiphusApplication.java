a77e949b93ce550e12d38c8d724bde9ef7496c8d
Klaus Steinmeier <klaus.kstnmr@gmail.com>
2024-02-04 17:14:27 -0300

    Initial structure

---
+++ src/main/java/app/sysiphus/SysiphusApplication.java
@@ -0,0 +1,13 @@
+package app.sysiphus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysiphusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysiphusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/sysiphus/SysiphusApplicationTests.java
@@ -0,0 +1,13 @@
+package app.sysiphus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysiphusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
